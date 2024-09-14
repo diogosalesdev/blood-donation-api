63f60253fd2bcec1b3c09778ddddd9378499c90c
Diogo Sales <diogosalesdev@gmail.com>
2024-09-14 17:45:10 -0300

    feat: Add Swagger property tags

--- src/campaign/dto/create-campaign.dto.ts
+++ src/campaign/dto/create-campaign.dto.ts
@@ -1,24 +1,49 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { IsDateString, IsNotEmpty, IsString } from 'class-validator';
 
 export class CreateCampaignDTO {
+  @ApiProperty({
+    description: 'Título da Campanha',
+    example: 'Doando pro carnaval',
+  })
   @IsString()
   @IsNotEmpty()
   title: string;
 
+  @ApiProperty({
+    description: 'Descrição da campanha',
+    example: 'Camapnha de doação para obter estoque para o carnaval',
+  })
   @IsString()
   @IsNotEmpty()
   description: string;
 
+  @ApiProperty({
+    description: 'Data de início da campanha',
+    example: '06-01-2025',
+  })
   @IsDateString()
   startDate: string;
 
+  @ApiProperty({
+    description: 'Data de encerramento da campanha',
+    example: '20-02-2025',
+  })
   @IsDateString()
   endDate: string;
 
+  @ApiProperty({
+    description: 'Local/Região de alcance da campanha',
+    example: 'Região metropolitana do Recife',
+  })
   @IsString()
   @IsNotEmpty()
   location: string;
 
+  @ApiProperty({
+    description: 'Id da clínica que criou a campanha',
+    example: '482ddf20-1464-4912-9f2c-7561a439b2f2',
+  })
   @IsString()
   @IsNotEmpty()
   clinicId: string;
--- src/clinic/dto/create-clinic.dto.ts
+++ src/clinic/dto/create-clinic.dto.ts
@@ -1,18 +1,35 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { IsEmail, IsNotEmpty, IsString } from 'class-validator';
 
 export class CreateClinicDTO {
+  @ApiProperty({
+    description: 'Nome da clínica',
+    example: 'Hemope',
+  })
   @IsString()
   @IsNotEmpty()
   name: string;
 
+  @ApiProperty({
+    description: 'Endereço da c;ínica',
+    example: 'rua Doutor José de Brito, n1200',
+  })
   @IsString()
   @IsNotEmpty()
   address: string;
 
+  @ApiProperty({
+    description: 'Número de telefone da clínica',
+    example: '81999999999',
+  })
   @IsString()
   @IsNotEmpty()
   phone: string;
 
+  @ApiProperty({
+    description: 'Email da clínica',
+    example: 'hemope@swagger.com',
+  })
   @IsEmail()
   @IsNotEmpty()
   email: string;
