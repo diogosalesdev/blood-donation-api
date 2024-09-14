5eeb2f9bdb5772b6a348c6774a695c9cd2bd458e
Diogo Sales <diogosalesdev@gmail.com>
2024-09-14 16:50:23 -0300

    feat: Add Swagger property tags

--- src/donor/dto/create-donor.dto.ts
+++ src/donor/dto/create-donor.dto.ts
@@ -1,26 +1,54 @@
-import { IsEmail, IsNotEmpty, IsString } from 'class-validator';
+import { ApiProperty } from '@nestjs/swagger';
+import { IsEmail, IsNotEmpty, IsString, Matches } from 'class-validator';
 
 export class CreateDonorDTO {
+  @ApiProperty({
+    description: 'Nome completo do Doador',
+    example: 'Diogo Sales',
+  })
   @IsString()
   @IsNotEmpty()
   name: string;
 
+  @ApiProperty({
+    description: 'Email do doador',
+    example: 'diogosales@swagger.com',
+  })
   @IsEmail()
   @IsNotEmpty()
   email: string;
 
+  @ApiProperty({
+    description: 'Número de telefone do doador',
+    example: '81999999999',
+  })
   @IsString()
   @IsNotEmpty()
   phone: string;
 
+  @ApiProperty({
+    description: 'Número do cpf do doador',
+    example: '11111111111',
+  })
   @IsString()
   @IsNotEmpty()
   cpf: string;
 
+  @ApiProperty({
+    description: 'Tipo sanguíneo do Doador',
+    example: 'O-',
+  })
   @IsString()
   @IsNotEmpty()
+  @Matches(/^(A|B|AB|O)[+-]$/, {
+    message: 'O tipo sanguíneo deve ser A+, A-, B+, B-, AB+, AB-, O+ ou O-',
+  })
   bloodType: string;
 
+  @ApiProperty({
+    description: 'Endereço do Doador',
+    example: 'rua da Concordia, 134',
+  })
   @IsString()
   @IsNotEmpty()
   address: string;
